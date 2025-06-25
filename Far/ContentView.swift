7fdd68ffef1e29d6a7f389a1d7e8dfeda6721e93
Austin Burgess <ahburgess22@gmail.com>
2025-06-25 18:48:42 -0400

    Initial commit: Adventure Keeper iOS app foundation

---
+++ Far/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Far
+//
+//  Created by Austin Burgess on 6/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Far/FarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarApp.swift
+//  Far
+//
+//  Created by Austin Burgess on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
